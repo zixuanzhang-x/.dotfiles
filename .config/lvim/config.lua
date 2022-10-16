f2075d1c1b22f5d6c03ef3f9bbf32175cd9b6859
Zixuan Zhang <zixuanzhang@PikachuStation>
2022-10-16 15:57:08 -0400

    Comment out toggle terminal

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -23,7 +23,7 @@ lvim.leader = "space"
 -- lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
-lvim.keys.normal_mode["<C-t>"] = ":ToggleTerm<CR>"
+-- lvim.keys.normal_mode["<C-t>"] = ":ToggleTerm<CR>"
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
