bca1ad71ce8908ff2fb2e44c589f5a22c7422393
Zixuan Zhang <zixuanzhang.x@gmail.com>
2022-10-16 11:25:05 -0400

    Add symlink commands

---
+++ symlinks.sh
@@ -0,0 +1,4 @@
+    ln -sf ~/Projects/.dotfiles/.zshrc .zshrc
+    ln -sf ~/Projects/.dotfiles/.tmux.conf .tmux.conf
+    ln -sf ~/Projects/.dotfiles/.ideavimrc .ideavimrc
+    # ln -s ~/Projects/.dotfiles/.vimrc .vimrc
