19c47f1078f055624c8adec4730eaaf19181c1f8
Zixuan Zhang <zixuanzhang.x@gmail.com>
2022-11-03 00:39:48 -0400

    Fix symlink.sh (replace target symlink and prompt when doing so)

--- symlinks.sh
+++ symlinks.sh
@@ -1,8 +1,9 @@
-    ln -sf ~/Projects/.dotfiles/.zshrc ~/.zshrc
-    ln -sf ~/Projects/.dotfiles/.tmux.conf ~/.tmux.conf
-    ln -sf ~/Projects/.dotfiles/.ideavimrc ~/.ideavimrc
-    ln -sf ~/Projects/.dotfiles/.tool.toml ~/.tool.toml
-    ln -sf ~/Projects/.dotfiles/.vimrc ~/.vimrc
-    ln -sf ~/Projects/.dotfiles/.config/nvim ~/.config/nvim
-    ln -sf ~/Projects/.dotfiles/.config/helix ~/.config/helix
-    ln -sf ~/Projects/.dotfiles/.config/lvim/config.lua ~/.config/lvim/config.lua
+ln -sfhi ~/Projects/.dotfiles/.zshrc ~/.zshrc
+ln -sfhi ~/Projects/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfhi ~/Projects/.dotfiles/.ideavimrc ~/.ideavimrc
+ln -sfhi ~/Projects/.dotfiles/.tool.toml ~/.tool.toml
+ln -sfhi ~/Projects/.dotfiles/.vimrc ~/.vimrc
+# ln -sf ~/Projects/.dotfiles/.config/nvim ~/.config/nvim
+ln -sfhi ~/Projects/.dotfiles/.config/helix ~/.config/helix
+ln -sfhi ~/Projects/.dotfiles/.config/lvim/config.lua ~/.config/lvim/config.lua
+ln -sfhi ~/Projects/.dotfiles/astronvim_config ~/.config/nvim/lua/user
