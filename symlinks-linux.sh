08522977e8268e3f82b385f64e73f41698d20b3a
Zixuan Zhang <zixuanzhang@PikachuStation>
2022-12-30 15:20:23 -0500

    Make symlinks.sh platform specific becuase of the implementation difference of the ln command

---
+++ symlinks-linux.sh
@@ -0,0 +1,10 @@
+ln -sfi ~/Projects/.dotfiles/.zshrc ~/.zshrc
+# ln -sfi ~/Projects/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfi ~/Projects/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
+ln -sfi ~/Projects/.dotfiles/.ideavimrc ~/.ideavimrc
+ln -sfi ~/Projects/.dotfiles/.tool.toml ~/.tool.toml
+ln -sfi ~/Projects/.dotfiles/.vimrc ~/.vimrc
+ln -sfiT ~/Projects/.dotfiles/astronvim_config ~/.config/nvim/lua/user
+# ln -sfi ~/Projects/.dotfiles/.config/nvim ~/.config/nvim
+# ln -sfi ~/Projects/.dotfiles/.config/helix ~/.config/helix
+# ln -sfi ~/Projects/.dotfiles/.config/lvim/config.lua ~/.config/lvim/config.lua
---
+++ symlinks-osx.sh
@@ -0,0 +1,10 @@
+ln -sfhi ~/Projects/.dotfiles/.zshrc ~/.zshrc
+# ln -sfhi ~/Projects/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sfhi ~/Projects/.dotfiles/.tmux.conf.local ~/.tmux.conf.local
+ln -sfhi ~/Projects/.dotfiles/.ideavimrc ~/.ideavimrc
+ln -sfhi ~/Projects/.dotfiles/.tool.toml ~/.tool.toml
+ln -sfhi ~/Projects/.dotfiles/.vimrc ~/.vimrc
+ln -sfhi ~/Projects/.dotfiles/astronvim_config ~/.config/nvim/lua/user
+# ln -sfhi ~/Projects/.dotfiles/.config/nvim ~/.config/nvim
+# ln -sfhi ~/Projects/.dotfiles/.config/helix ~/.config/helix
+# ln -sfhi ~/Projects/.dotfiles/.config/lvim/config.lua ~/.config/lvim/config.lua
